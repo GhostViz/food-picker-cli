5b4b7f957adc62ac382e0e5d7b7608bbba1dc3b8
Joe Nichols <joe.nichols@ibm.com>
2017-10-21 15:27:08 -0500

    first commit

---
+++ app.rb
@@ -0,0 +1,97 @@
+require "json"
+require "http"
+require "optparse"
+
+
+# Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+# from https://www.yelp.com/developers/v3/manage_app
+CLIENT_ID = nil
+CLIENT_SECRET = nil
+
+# Constants, do not change these
+API_HOST = "https://api.yelp.com"
+SEARCH_PATH = "/v3/businesses/search"
+TOKEN_PATH = "/oauth2/token"
+GRANT_TYPE = "client_credentials"
+
+DEFAULT_TERM = "food"
+DEFAULT_LOCATION = "75240"
+DEFAULT_SEARCH_LIMIT = 10
+DEFAULT_DISTANCE = 2.5
+
+def bearer_token
+
+  url = "#{API_HOST}#{TOKEN_PATH}"
+
+  raise "Please set your CLIENT_ID" if CLIENT_ID.nil?
+  raise "Please set your CLIENT_SECRET" if CLIENT_SECRET.nil?
+
+  params = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    grant_type: GRANT_TYPE
+  }
+
+  response = HTTP.post(url, params: params)
+  parsed = response.parse
+
+  "#{parsed['token_type']} #{parsed['access_token']}"
+end
+
+def findPlaces(term, location, searchLimit)
+  url = "#{API_HOST}#{SEARCH_PATH}"
+  params = {
+    term: term,
+    location: location,
+    sort_by: "rating",
+    price: "1",
+    radius: 4825,
+    open_now: true,
+    limit: searchLimit
+  }
+
+  response = HTTP.auth(bearer_token).get(url, params: params)
+  response.parse
+end
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Example usage: ruby sample.rb search [options]"
+
+  opts.on("-tTERM", "--term=TERM", "Search term") do |term|
+    options[:term] = term
+  end
+
+  opts.on("-lLOCATION", "--location=LOCATION", "Search location") do |location|
+    options[:location] = location
+  end
+
+  opts.on("-sSEARCH_LIMIT", "--limit=LIMIT", "Search limit") do |searchLimit|
+    options[:searchLimit] = searchLimit
+  end
+
+  opts.on("-h", "--help", "Prints this help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+term = options.fetch(:term, DEFAULT_TERM)
+location = options.fetch(:location, DEFAULT_LOCATION)
+searchLimit = options.fetch(:searchLimit, DEFAULT_SEARCH_LIMIT).to_i
+
+response = findPlaces(term, location, searchLimit)
+
+if response["total"] < searchLimit
+  searchLimit = response["total"]
+end
+
+puts "Picking from top #{searchLimit.to_s} results of #{response["total"]} total \nfor the term \"#{term}\" in location \"#{location}\""
+puts "-----"
+randomNumber = rand(searchLimit+1)-1
+pickedBusiness = response["businesses"][randomNumber]
+puts pickedBusiness["name"]
+puts pickedBusiness["location"]["address1"].to_s + ' ' + pickedBusiness["location"]["address2"].to_s + ' ' + pickedBusiness["location"]["address3"].to_s
+puts pickedBusiness["location"]["city"].to_s + ', ' + pickedBusiness["location"]["state"].to_s + ' ' + pickedBusiness["location"]["zip_code"].to_s
+puts "Distance: %.1f miles" % (pickedBusiness["distance"]/1609.344)
+puts "Rating: " + pickedBusiness["rating"].to_s
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Food Picker CLI
+
+This is a food picker command line interface using the Yelp Fusion API. This is written in Ruby and should be compatible with Ruby 2.2 and higher.
+I used the yelp-fusion repo as a base for this so I could learn some  of the basics of the Yelp Fusion API. (https://github.com/Yelp/yelp-fusion.git)
+
+## Setup
+
+Clone the repo:
+
+```
+$ git clone git@github.com:GhostViz/food-picker-cli.git
+```
+
+Go into the directory:
+
+```
+$ cd food-picker-cli
+```
+
+Install the dependences:
+
+```
+$ bundle
+```
+
+Open up the `app.rb` file and put your client_id and client_secret from the [manage app page](https://www.yelp.com/developers/v3/manage_app)
+
+```
+# Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+# from https://www.yelp.com/developers/v3/manage_app
+CLIENT_ID = <YOUR_CLIENT_ID>
+CLIENT_SECRET = <YOUR_CLIENT_SECRET>
+```
+
+## Usage
+
+Once you're all setup, you should be able to run the script directly from the command line:
+```
+$ ruby app.rb
+```
+The above will run using the defaults term=food, location=75240, and limit=10
+
+To specify your own location, term and limit you can use the following parameters
+
+```
+$ ruby app.rb --term="TERM" --location="LOCATION" --limit="NUMBER"
+```
+
+``` --term ``` is the keyword you are searching for. Lunch, Breakfast, Mexican, Chinese, etc
+``` --location ``` is either your city, state or zipcode
+``` --limit ``` is the number of results you'd like your random selection to be picked from
